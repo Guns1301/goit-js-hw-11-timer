90f1fe7837e5b431b70b48dcbbcbd0e68a07977e
Guns1301 <67137938+Guns1301@users.noreply.github.com>
2021-04-22 23:42:48 +0300

    add timer

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,1 +1,27 @@
-import refs from '.refs.js';
+import refs from './refs.js';
+
+let day = `Jan 01 2021`;
+
+function setTime(time) {
+  const days = Math.floor(time / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  const secs = Math.floor((time % (1000 * 60)) / 1000);
+  return { days, hours, mins, secs };
+}
+
+function myTimer(date) {
+  let targetDate = new Date(date);
+  let currentDate = Date.now();
+  let timeBetween = targetDate - currentDate;
+  let myDate = setTime(timeBetween);
+
+  refs.daysRef.textContent = myDate.days;
+  refs.hoursRef.textContent = myDate.hours;
+  refs.minsRef.textContent = myDate.mins;
+  refs.secsRef.textContent = myDate.secs;
+}
+
+let interval = setInterval(() => {
+  myTimer(day);
+}, 1000);
--- src/js/timerByClass.js
+++ src/js/timerByClass.js
@@ -1,1 +1,49 @@
 import refs from './refs.js';
+
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.intervalId = null;
+  }
+
+  count(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    if (time > 0) {
+      refs.daysRef.textContent = days;
+      refs.hoursRef.textContent = hours;
+      refs.minsRef.textContent = mins;
+      refs.secsRef.textContent = secs;
+    } else clearInterval(this.intervalId);
+  }
+
+  timer() {
+    let currentDate = Date.now();
+    const deltaTime = this.targetDate - currentDate;
+    this.count(deltaTime);
+  }
+
+  start() {
+    this.timer();
+    this.intervalId = setInterval(() => {
+      this.timer();
+    }, 1000);
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+}
+
+const myTimer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Aug 28, 2021'),
+});
+
+myTimer.start();

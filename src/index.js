35dd1f5889872d09617874870d7fb276925ce8a5
Guns1301 <67137938+Guns1301@users.noreply.github.com>
2021-04-22 22:22:28 +0300

    add webpack, index html js css

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './styles.css';
+import './js/timer.js';
+import './js/timerByClass.js';
---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+export default {
+  timer: document.getElementById('timer-1'),
+  daysRef: document.querySelector('[data-value="days"]'),
+  hoursRef: document.querySelector('[data-value="hours"]'),
+  minsRef: document.querySelector('[data-value="mins"]'),
+  secsRef: document.querySelector('[data-value="secs"]'),
+};
